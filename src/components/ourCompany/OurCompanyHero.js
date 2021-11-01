6f30cc0b0fd60216a60b07b0fd42fbd5f8605889
habib <habibdev96@gmail.com>
2021-11-01 04:37:35 -0600

    fixed build errors

--- src/components/ourCompany/OurCompanyHero.js
+++ src/components/ourCompany/OurCompanyHero.js
@@ -69,7 +69,7 @@ export default function OurCompanyHero() {
               results for our clients. We’ve partnered with many startups,
               corporations, and nonprofits alike to craft designs that make real
               impact. We’re always looking forward to creating brands, products,
-              and digital experiences that connect with our clients' audiences.
+              and digital experiences that connect with our clients audiences.
             </Paragraph>
           </div>
           <div className='showcase'>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Globals from '../abstracts/Globals';
 import { AppProvider } from '../context/context';
 import Navbar from '../components/shared/Navbar';
@@ -8,15 +7,6 @@ import Footer from '../components/shared/Footer';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Jost:wght@400;500&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
-
       <AppProvider>
         <Globals />
         <Navbar />
